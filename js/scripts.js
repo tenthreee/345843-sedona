8464086244424826716add5409cc0b5f3573565d
Lena Petelina <lena.petelina@gmail.com>
2017-02-13 18:33:57 +0300

    Сделала правки

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 var link = document.querySelector(".hotel-search-btn");
 var form = document.querySelector(".hotel-search-form");
 
+form.classList.add("hotel-search-form-hide");
+
 link.addEventListener("click", function (event) {
   event.preventDefault();
   form.classList.toggle("hotel-search-form-hide");
